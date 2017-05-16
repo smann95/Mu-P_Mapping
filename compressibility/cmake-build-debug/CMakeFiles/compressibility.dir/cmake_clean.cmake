de7a5e4b34ab996837e9ba433bd5c3dfb9c7d55a
Luciano Laratelli <luciano.e.laratelli@outlook.com>
2017-05-16 18:03:30 -0400

    Add compressibility stuff

---
+++ compressibility/cmake-build-debug/CMakeFiles/compressibility.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/compressibility.dir/main.cpp.o"
+  "compressibility.pdb"
+  "compressibility"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/compressibility.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
