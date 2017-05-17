379febee319293416a7ae3ebf733efaac8f0a3db
Luciano Laratelli <luciano.e.laratelli@outlook.com>
2017-05-17 15:17:34 -0400

    Add MPMC Co2 fugacity function and modify to return compressibility instead

---
+++ compressibility/calculations.cpp
@@ -0,0 +1,12 @@
+//
+// Created by Luciano Laratelli on 17/05/2017.
+//
+
+#include "compressibility.h"
+
+double get_simulation_compressibility(double temperature, double pressure)
+{
+    double sim_compressibility = 0;
+    return sim_compressibility;
+}
+
--- compressibility/compressibility.h
+++ compressibility/compressibility.h
@@ -5,4 +5,10 @@
 #ifndef COMPRESSIBILITY_COMPRESSIBILITY_H
 #define COMPRESSIBILITY_COMPRESSIBILITY_H
 
+#include <iostream>
+#include <vector>
+
+
+double get_state_compressibility(double temperature, double pressure);
+double get_simulation_compressiblity(double temperature, double pressure);
 #endif //COMPRESSIBILITY_COMPRESSIBILITY_H
