60fbaf9b4935749acf3568e3e77d8eb4044938bc
Luciano Laratelli <luciano.e.laratelli@outlook.com>
2017-05-25 19:24:02 -0400

    Add h2 stuff

--- compressibility/doit.sh
+++ compressibility/doit.sh
@@ -2,7 +2,7 @@
 
 datestamp=$(date +'_%m_%d_%Y')
 
-for species in co2 n2; do
+for species in co2 n2 h2; do
     scp laratelli@itn.rc.usf.edu:~/muP_mapping/${species}${datestamp}".dat" .
     cmake-build-debug/compressibility ${species}${datestamp}".dat"
     mkdir -p ${species} GRAPHS
@@ -11,11 +11,13 @@ for species in co2 n2; do
     cd ..
 done
 
-split -l 11 "co2"${datestamp}".dat.OUT" DATA
-cd co2/
-rm DATAa*
-cd ..
-mv DATAa* co2/
+for species in co2 h2;do
+    split -l 11 ${species}${datestamp}".dat.OUT" DATA
+    cd ${species}
+    rm DATAa*
+    cd ..
+    mv DATAa* ${species}
+done
 
 split -l 12 "n2"${datestamp}".dat.OUT" DATA
 cd n2/
@@ -23,7 +25,7 @@ rm DATAa*
 cd ..
 mv DATAa* n2/
 
-for species in co2 n2; do
+for species in co2 n2 h2; do
     cd ${species}
         echo "Starting $species graphs now..."
         python graphout.py DATAaa
---
+++ compressibility/h2/graphout.py
@@ -0,0 +1,48 @@
+import sys
+import pandas
+import matplotlib.pyplot as plt
+import numpy
+data = numpy.loadtxt(sys.argv[1], delimiter=',')
+column_names = ['t', 'p', 'sim_z', 'eos_z', 'simulation_f', 'eos_f']
+the_data = pandas.read_csv(sys.argv[1], names=column_names)
+
+t = the_data.t.tolist()
+p = the_data.p.tolist()
+state_z = the_data.eos_z.tolist()
+sim_z = the_data.sim_z.tolist()
+
+fig = plt.figure()
+
+
+plt.scatter(t, state_z, s=20, c='red')
+plt.plot(t, state_z, c='red', label='State Z')
+plt.scatter(t, sim_z, s=20, c='green')
+plt.plot(t, sim_z, c='green', label='Simulation Z')
+
+title = "NPT "
+file_name = "file"
+if sys.argv[1] == "DATAaa":
+    title += "H2 (BSSP) at P = 0.1"
+    file_name = "H2_P_0.1.png"
+elif sys.argv[1] == "DATAab":
+    title += "H2 (BSSP) at P = 1.0"
+    file_name = "H2_P_1.png"
+elif sys.argv[1] == "DATAac":
+    title += "H2 (BSSP) at P = 5.0"
+    file_name = "H2_P_5.png"
+elif sys.argv[1] == "DATAad":
+    title += "H2 (BSSP) at P = 10.0"
+    file_name = "H2_P_10.png"
+elif sys.argv[1] == "DATAae":
+    title += "H2 (BSSP) at P = 20.0"
+    file_name = "H2_P_20.png"
+elif sys.argv[1] == "DATAaf":
+    title += "H2 (BSSP)  at P = 30.0"
+    file_name = "H2_P_30.png"
+
+plt.title(title)
+plt.legend(loc='center left',bbox_to_anchor=(0,1))
+plt.xlabel('Temperature')
+plt.ylabel('Compressibility')
+
+plt.savefig(file_name, bbox_inches='tight')
