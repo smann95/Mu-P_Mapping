2e3a62fae0c2642bb0349a965d5284f0b4bb1f3d
Luciano Laratelli <luciano.e.laratelli@outlook.com>
2017-06-02 12:50:41 -0400

    Rename doit to be (a little) more descriptive and to only use one loop to do everything

---
+++ compressibility/data_scrape_and_analysis.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+trap "[ -z "$ctl" ] || ssh -S $ctl -O exit $user@$host" EXIT # closes conn, deletes fifo
+
+read -p "Host to connect to? " host
+read -p "User to login with? " user
+
+BOLD="\n$(tput bold)"; NORMAL="$(tput sgr0)"
+echo -e "${BOLD}Create authenticated persistent control master connection:${NORMAL}"
+sshfifos=~/.ssh/controlmasters
+[ -d ${sshfifos} ] || mkdir -p $sshfifos; chmod 755 ${sshfifos}
+ctl=${sshfifos}/${user}@${host}:22 # ssh stores named socket for open ctrl conn here
+
+ssh -fNMS $ctl $user@$host  # Control Master: Prompts password then persists in background
+
+
+date_stamp=$(date +'_%m_%d_%Y')
+mkdir -p GRAPHS ; cd GRAPHS ; rm *.png ; cd ..
+
+CH4_models=( 9_site_nonpolar 9_site_polar trappe )
+CO2_models=( PHAST TRAPPE PHAST_STAR )
+H2_models=( BSS BSSP BUCH DL POLAR_ADJQ)
+N2_models=( TRAPPE PHAST_STAR_LB )
+
+for species in CH4 CO2 H2 N2; do
+    array="${species}_models[@]"
+    mkdir -p ${species}
+    cd ${species}
+    for model in "${!array}"; do
+        mkdir -p ${model}
+        cd .. #out of species into the main directory
+        scp -o ControlPath=${ctl} ${user}@${host}:~/muP_mapping/${species}${model}${date_stamp}".dat" .
+        cmake-build-debug/compressibility ${species}${model}${date_stamp}".dat"
+        split -l 11 ${species}${model}${date_stamp}".dat.OUT" DATA
+        cd ${species}/${model} #into the innermost directory (species + model)
+        rm DATAa*
+        cd ../../ #to the outermost directory (the main working directory)
+        mv DATAa* ${species}/${model}
+        cd ${species}/${model} #into the innermost directory (species + model)
+        echo "Starting $species($model) graphs now..."
+        python graphout.py DATAaa
+        python graphout.py DATAab
+        python graphout.py DATAac
+        python graphout.py DATAad
+        python graphout.py DATAae
+        python graphout.py DATAaf
+        mv *.png ../../GRAPHS
+        cd ../ #back to species
+    done
+    cd ..
+done
