fd0439e15d1422fdc8fd66fb644a2709cea1e694
Luciano Laratelli <luciano.e.laratelli@outlook.com>
2017-06-02 08:03:51 -0400

    Add N2 to make all dirs

--- compressibility/make_all_dirs.sh
+++ compressibility/make_all_dirs.sh
@@ -11,7 +11,7 @@ for species in H2; do
     mkdir -p ${species}
     cd ${species}
         touch these_numbers_are_pressures_in_ATM
-        for pres in 0.1 1.0 5.0 10.0 20.0 30.0; do
+        for pres in 00.1 01.0 05.0 10.0 20.0 30.0; do
             mkdir -p ${pres}
             cd ${pres}
             touch these_are_temperatures_in_kelvin
@@ -48,7 +48,7 @@ for species in CO2; do
     mkdir -p ${species}
     cd ${species}
         touch these_numbers_are_pressures_in_ATM
-        for pres in 0.1 1.0 5.0 10.0 20.0 30.0; do
+        for pres in 00.1 01.0 05.0 10.0 20.0 30.0; do
             mkdir -p ${pres}
             cd ${pres}
             touch these_are_temperatures_in_kelvin
@@ -85,11 +85,48 @@ for species in CH4; do
     mkdir -p ${species}
     cd ${species}
         touch these_numbers_are_pressures_in_ATM
-        for pres in 0.1 1.0 5.0 10.0 20.0 30.0; do
+        for pres in 00.1 01.0 05.0 10.0 20.0 30.0; do
             mkdir -p ${pres}
             cd ${pres}
             touch these_are_temperatures_in_kelvin
             for temp in 115.0 145.0 175.0 205.0 235.0 265.0 295.0 325.0 355.0 385.0 415.0; do
+                mkdir -p ${temp}
+                cd ${temp};
+                for ensemble in npt; do
+                    mkdir -p ${ensemble}
+                    cd ${ensemble}
+                    for model in 9_site_nonpolar 9_site_polar _trappe; do
+                        mkdir -p ${model}
+                        cd ${model}
+                        awk -v pres="$pres" -v temp="$temp" -v ensemble="$ensemble" -v species="$species" '{
+                        gsub(/XXXENSEMBLEXXX/, ensemble);
+                        gsub(/XXXTEMPXXX/, temp);
+                        gsub(/XXXPRESXXX/, pres);
+                        gsub(/XXXSPECIESXXX/, species);
+                        print;
+                        }' ${current}/inputfiles/${ensemble}"H2input.inp" > ${species}".inp"
+                        cp ${current}/modelfiles/FOURBYFOUR/${species}_${model}".pqr" input.pqr
+                        cd .. #out of model
+                    done
+                    cd .. #out of ensemble
+                done
+                cd .. #out of temperature
+            done
+            cd .. #out of pressure
+        done
+    cd .. #out of species
+done
+
+for species in N2; do
+    echo "Making directories for $species..."
+    mkdir -p ${species}
+    cd ${species}
+        touch these_numbers_are_pressures_in_ATM
+        for pres in 00.1 01.0 05.0 10.0 20.0 30.0; do
+            mkdir -p ${pres}
+            cd ${pres}
+            touch these_are_temperatures_in_kelvin
+            for temp in  077.0 110.0 140.0 170.0 200.0 230.0 260.0 290.0 330.0 370.0 410.0; do
                 mkdir -p ${temp}
                 cd ${temp};
                 for ensemble in npt; do
