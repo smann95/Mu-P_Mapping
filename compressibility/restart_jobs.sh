ec64585befa83dbf11c0583b6fda88f4bc5958f8
Luciano Laratelli <luciano.e.laratelli@outlook.com>
2017-06-08 13:17:12 -0400

    Yikes

--- compressibility/restart_jobs.sh
+++ compressibility/restart_jobs.sh
@@ -43,7 +43,7 @@ for species in CH4 CO2 N2 NE AR KR XE; do
                             cd ${temperature}
                                 job_number=$(ls *.out | sed 's/^[^-]*-\([^.]*\).*/\1/')
                                 submitted=$(squeue -u $USER | grep -q "${job_number}")
-                                if [ "$?" -eq "0" ]; then
+                                if [ "$?" -ne "0" ]; then
                                     mkdir -p saved_data
                                     mv input.pqr saved_data/old_input.pqr
                                     if [ -s *restar*.pqr ]; then
@@ -84,7 +84,7 @@ for species in H2 HE; do
                                     cd ${temperature}
                                         job_number=$(ls *.out | sed 's/^[^-]*-\([^.]*\).*/\1/')
                                         submitted=$(squeue -u $USER | grep -q "${job_number}")
-                                        if [ "$?" -eq "0" ]; then
+                                        if [ "$?" -ne "0" ]; then
                                         mkdir -p saved_data
                                             mv input.pqr saved_data/old_input.pqr
                                             if [ -s *restar*.pqr ]; then
